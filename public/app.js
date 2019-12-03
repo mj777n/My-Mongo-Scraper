251232ed6cb15c8be0de5d8c5cde095e1c0c791f
M N <mj777norato@gmail.com>
2019-12-03 17:25:05 -0500

    Adding comments and cleaning up code blocks in app, routes

--- public/app.js
+++ public/app.js
@@ -12,14 +12,15 @@ $(document).ready(function() {
   $("#scrape").on("click", function(event) {
     event.preventDefault();
     $.getJSON("/scrape", function() {
+      location.reload();  // not exactly working as hoped
     });
       console.log("Scrape completed!");
-      location.reload();  
+      location.reload(); // not exactly working as hoped
+      // Still have to click home or refresh to see scraped articles rendered
   });
-        
+       
     // WORKING "Clear Articles" button, app.get("/drop-collection")
     // Click event for deleting the collection from the db
-    // $("#clear").on("click", event => {
   $("#clear").on("click", function(event) {
     event.preventDefault();
     $.getJSON("/drop-collection", function() {
@@ -29,14 +30,13 @@ $(document).ready(function() {
     $("#display").empty();
   });
         
-    //  PUT PUT PUT a saved article in
+    //  PUT a saved article/ render to browser in Saved Articles page
     // WORKING "Save Article" button: app.put("/saved/:id")
     // Click event to use PUT route with ID to save article clicked on
   $(document).on("click", ".saved", function () {
+      // Grab the ID of the element(article) clicked
     var id = $(this).attr("id");
     saveId = id;
-    console.log("saveId = "+saveId);
-
     $(this).parents(".card").remove();
     $.ajax({
       type: "PUT",
@@ -48,82 +48,67 @@ $(document).ready(function() {
   });
 });
 
-// ***************  PENDING CODE **************
-  $(".addComment").on("click", function () { 
-  // Empty the value of the user box from prior notes entered in another note box
-$("#userNote").val("");
-  // Save the id from the button   
-var id = $(this).attr("id");
-console.log("var id from .addComment = "+id); // this works
-// console.log("title is: "+data.title);
-  // Now make an ajax call for the Article   
-$.ajax({
-  method: "GET",   
-  url: "/articles/" + id, 
-})
+  // WORKING "Comment" button, app.get("/"/articles/:id")
+  // Click event for adding a comment to a saved article
+$(".addComment").on("click", function () { 
+    // Empty the value of the user box from prior notes entered in another note box
+  $("#userNote").val("");
+   // Grab the ID of the element(article) clicked 
+  var id = $(this).attr("id");
+  console.log("var id from .addComment = "+id); // this works
+    // Now make an ajax call for the Article with the ID  
+  $.ajax({
+    method: "GET",   
+    url: "/articles/" + id, 
+  })
     // Next, add the note information to the page
   .then(function (data) {
     console.log(data);  // working
-    console.log("Same ID from .addComment should be "+id);
-    // $("#noteBox").empty();
     $("#userNote").empty();
     $(".modal-title").empty();
     $(".modal-footer").empty(); 
-      // The title of the article
+      // Add (append) The title of the article to the modal box
     $(".modal-title").append("<h4>" + data.title + "</h3>");
-      // If there's a note in the article
-    // var button = 
-// A button to submit a new note, with the id of the article saved to it
-// $(".modal-footer").append("<button data-id='"+ data._id +"' id='savenote'>Save Note</button>");
-
-if (data.note) {
-        // Place the body of the note in the body textarea
-        console.log("PRIOR note found in article");
-        $("#userNote").val(data.note.body);
-    $(".modal-footer").empty();  
-    // $(".modal-footer").val("");     
-}
-$(".modal-footer").append("<button class='btn btn-secondary btn-sm' data-dismiss='modal'>Close</button>");
-$(".modal-footer").append("<button class='btn btn-primary btn-sm' data-id='"+ data._id +"' id='savenote' data-dismiss='modal'>Save Note</button>");   
-  });
-});
-
-// SAVE NOTE --- POST 
-// When you click the savenote button
-$(document).on("click", "#savenote", function() {
-    // $(".saveNote").on("click", function () {  
-// $("#savenote").on("click", function () {   
-var id = $(this).attr("data-id");
-        // var id = $(this).attr("id");
-        console.log("In POST: id = "+id);
-    // Run a POST request to change the note, using what's entered in the inputs 
-  $.ajax({
-    method: "POST",      
-    url: "/notes/" + id,           
-    data: {
-      body: $("#userNote").val().trim()
+      // Check If there's already a note in the article
+    if (data.note) { 
+        // If so, place the body of the existing note in the body textarea
+      console.log("PRIOR note found in article");
+      $("#userNote").val(data.note.body);
+        // Empty the modal footer of buttons
+      $(".modal-footer").empty();    
     }
-  }).then(function(data) {
-   console.log(data);  
-    console.log("data._id below:"); 
-      console.log(data._id); 
-      // Empty the notes section
-    $("#userNote").empty();
-    $(".modal-footer").empty();
-  }); 
-    // Also, remove the values entered in the input and textarea for note entry
-  $("#userNote").val("");
-  $(".savedUserNote").val("");
-  $(".modal-footer").val("");
-});
+      // Add (append) the 2 buttons needed into the modal footer
+      // Key to ensuring the ID is captured on the element when clicked
+      // Click "Save Note" to create(submit) a new note, with the id of the article saved to it
+    $(".modal-footer").append("<button class='btn btn-secondary btn-sm' data-dismiss='modal'>Close</button>");
+    $(".modal-footer").append("<button class='btn btn-primary btn-sm' data-id='"+ data._id +"' id='saveNote' data-dismiss='modal'>Save Note</button>");   
+  });  // close .then
+});  // close "Add comment" button/function block
 
-  // console.log(data);
-  // $(".saveNote").attr("data-id", id);
-  // $(".saveComment").attr("data-id", id);  
-  // $(".article").attr("data-id", id);
-  // });
-  // $(".savedNote").empty();
-  //  });
+  // SAVE NOTE --- POST 
+  // Triggered When you click the savenote button
+$(document).on("click", "#saveNote", function() { 
+    // Grab the ID of the element(article) clicked
+  var id = $(this).attr("data-id");
+    // Run a POST request to create a new note in the Notes collection, using what's entered in the inputs 
+    // Passing the ID and the entered userNotes (in data field)
+    $.ajax({
+      method: "POST",      
+      url: "/notes/" + id,           
+      data: {
+        body: $("#userNote").val().trim()
+      }
+    }).then(function(data) {
+      console.log(data);  
+        // Empty the notes section
+      $("#userNote").empty();
+      $(".modal-footer").empty();
+    }); 
+      // Also, remove the values entered in the input and textarea for note entry
+    $("#userNote").val("");
+    $(".savedUserNote").val("");
+    $(".modal-footer").val("");
+});
 
   // WORKING "Delete Article" button: app.delete("/delete-Article/id")
   // Click event on "Saved Articles" page to delete a saved article 
@@ -132,19 +117,9 @@ $(document).on("click", "button.delete", function (event) {
   let id = $(this).attr('id');
   $.ajax({
     url: "/delete-Article/" + id,
-      //   method: "DELETE",
     method: "DELETE"
   })
   .then(function (data) {
     location.reload();
   });
 });
-    // Is this "delete" being used??
-      // $(".deleteArticle").on("click", function () {
-      //   let id = $(this).attr("id");
-      //   $.ajax("/delete-Article/" + id, {
-      //     type: "DELETE"
-      //   }).then(function() {
-      //     location.reload();
-      //   });
-      // }); 
--- routes/routes.js
+++ routes/routes.js
@@ -3,11 +3,14 @@ let cheerio = require("cheerio");
 var db = require("../models");
 
 module.exports = function (app) {
-
+    // WORKING "SCRAPE"
   app.get("/scrape", function (req, res) {
     axios.get("https://www.nytimes.com/section/technology").then(function (response) {
       var $ = cheerio.load(response.data);
+        // Look for List Items of articles
+        // Grab the anchor, header(2) and paragraph tags
       $("li").each(function (i, element) {           
+          // Set up a var array for storing 
         let result = {};
         result.link = $(element).find("a").attr("href");  
         result.title = $(element).find("h2").text();                            
@@ -26,12 +29,14 @@ module.exports = function (app) {
           .catch(function(err) {
             console.log(err);
           }); // close .catch               
-      } ); // close .each
+      }); // close .each
+      // location.reload();  
       res.send("The SCRAPE was successful")       
     }); // close AXIOS.GET
   }); // close "scrape"
+  // location.reload(); 
 
-    // GOT IT
+    // WORKING
     // Route for home page
   app.get("/", function (req, res) {
     db.Article.find({}, function (err, data) {
@@ -43,7 +48,7 @@ module.exports = function (app) {
     })
   });
 
-    // GOT IT
+    // WORKING
     // Route for getting all Articles from the db
   app.get("/articles", function (req, res) {
      // Grab every document in the Articles collection      
@@ -58,16 +63,16 @@ module.exports = function (app) {
     });                            
   });
 
-    // GOT IT
+    // WORKING
     // Route for getting all Notes from the db
   app.get("/comments", function (req, res) {
     db.Note.find({}, function (error, data) {
         console.log(data)
         res.json(data);
     });
-});
+  });
 
-  // GOT IT
+    // WORKING
     // Route to render all the "Saved" articles
   app.get("/saved", function (req, res) {
     db.Article.find({}, function (err, data) {
@@ -86,15 +91,14 @@ module.exports = function (app) {
     // WORKING: Called by .ajax({PUT"/saved/"+id})
   app.put("/saved/:id", function (req, res) {
     db.Article.findByIdAndUpdate({ _id: req.params.id }, 
-    {$set: { saved: true } 
-      //  console.log("req.params.id= ");    
+      {$set: { saved: true } 
     }).then(function (data) {
       res.json(data);
       console.log('\n'+'---> app.PUT: route "/saved/:id" Just saved this article titled: '+data.title)
-      console.log("Article ID = "+data._id);
     });
   });
 
+  // WORKING
   // find an existing article by id / populate note
   // Triggered when COMMENT button clicked
   app.get("/articles/:id", function (req, res) {
@@ -102,58 +106,49 @@ module.exports = function (app) {
       // Then populate all of the notes associated with it
     .populate("note")
     .then(function (dbArticle) {
-      // console.log(dbArticle);
       res.json(dbArticle);
-  console.log("Inside routes/GET ariticle by ID / COMMENT BUTTON");
-  console.log(req.params.id);
-  console.log('\n'+'---> app.GET: route "/articles/:id" Find the Saved Article and populate note field with ID from req.params');     
+      console.log('\n'+'---> app.GET: route "/articles/:id" Find the Saved Article and populate note field with ID from req.params');     
     })
     .catch(function (err) {
-      // If an error occurred, send it to the client
+        // If an error occurred, send it to the client
       res.json(err);
     });
   });
 
-    // Create a new note
+    // WORKING - Create a new note
     // Triggered on SAVE COMMENT button
-    // app.post("/notes/:id", function (req, res) {
-app.post("/notes/:id", function(req, res) {  
-    // Create a new note and pass the req.body to the entry 
-  db.Note.create(req.body)
-    .then(function (dbNote) { 
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. 
-      // Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User 
-      // Since the mongoose query returns a promise, we can chain another `.then` which receives the result of the query     
-    // console.log("in routes POST, line above return statment");
-      return db.Article.findOneAndUpdate({_id: req.params.id},        
-      // {$push: {note: dbNote._id }}, {new: true}); // WORKS!!! 
-      {note: dbNote._id});
-      // {new: true}); // WORKS ALSO       
-  })
-  .then(function (dbArticle) {
-      // If this was able to successfully update an Article, send it back to the client  
-    res.json(dbArticle);              
-    console.log('\n'+'---> app.POST: route "/notes" Saved note for article: '+dbArticle._id); 
-    console.log(dbArticle);  // NOW HAS THE NOTE FIELD SAVED CORRECTLY
-  })
-  .catch(function (err) {
-      // If an error occurred, send it to the client   
-    res.json(err);
+  app.post("/notes/:id", function(req, res) {  
+      // Create a new note and pass the req.body to the entry 
+    db.Note.create(req.body)
+      .then(function (dbNote) { 
+          // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. 
+          // Update the Article to be associated with the new Note
+          // Since the mongoose query returns a promise, we can chain another `.then` which receives the result of the query     
+        return db.Article.findOneAndUpdate({_id: req.params.id},        
+          {note: dbNote._id});
+      })
+      .then(function (dbArticle) {
+          // If this was able to successfully update an Article, send it back to the client  
+        res.json(dbArticle);              
+        console.log('\n'+'---> app.POST: route "/notes" Saved note for article: '+dbArticle._id); 
+        console.log(dbArticle);  // NOW THE NOTE FIELD SAVED CORRECTLY
+      })
+      .catch(function (err) {
+        // If an error occurred, send it to the client   
+      res.json(err);
+    });
   });
-});
   
     // BEGIN DELETES //
     // Route triggered when "Clear Articles" clicked
-    // Using method "deleteMany" on collection
+    // Using method "deleteMany" on Article collection
   app.get("/drop-collection", function (req, res) {
     db.Article.deleteMany({}, function (err, del) {
     });
     console.log('\n'+'---> app.GET: route "/drop-collection" Clear Articles button clicked'   );
-    res.send("res.send statement in drop-collection get");
   });
 
-    // Remove ("delete") a Saved article
+    // Remove ("delete") a Saved article by ID from user selected
   app.delete("/delete-Article/:id", function (req, res) {
     db.Article.findByIdAndUpdate({ _id: req.params.id },
     {
@@ -162,12 +157,5 @@ app.post("/notes/:id", function(req, res) {
         res.json(data);
     }); 
     console.log('\n'+'---> app.DELETE: route "/delete-Article/:id" You just deleted a saved article');
-  });
-
-  app.delete("/delete-comment/:id", function (req, res) {
-    db.Note.findByIdAndRemove(req.params.id, (err, comment) => {
-      if (err) return res.status(500).send(err);
-        return res.status(200).send();
-    });
   });
 };
