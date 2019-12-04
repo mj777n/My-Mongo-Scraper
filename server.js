b7f1186ecb2168e3b448c51fd87466e80133319f
M N <mj777norato@gmail.com>
2019-12-04 14:09:21 -0500

    completed readme file - ready to submit

--- server.js
+++ server.js
@@ -19,20 +19,21 @@ app.set("view engine", "handlebars");
 // Connect to the Mongo DB
 //******************************************* */
   // Connect to the Mongo DB using local host
-  // var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongo-scraperdb";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongo-scraperdb";
  
-    // Or connect via Heroku using mLabs addon. But this URI is not connecting.
-    // Geting a bunch of "H10" error messages from Heroku Logs. Ughhhhh!!!
+  // Or connect via Heroku using mLabs add-on. But this is not connecting.
+  // **** Geting a bunch of "H10" error messages from Heroku Logs. Ughhhhh!!! ****
 
-    var MONGODB_URI = process.env.MONGODB_URI || "mongodb://heroku_8j8v3k8b:RedSox15@ds251948.mlab.com:51948/heroku_8j8v3k8b";
-  // var MONGODB_URI = "mongodb://heroku_8j8v3k8b:RedSox15@ds251948.mlab.com:51948/heroku_8j8v3k8b";
+  // This is the connection driver recommended by mLabs. It contains correct username and password and syntax.
+  //  var MONGODB_URI = process.env.MONGODB_URI || "mongodb://heroku_8j8v3k8b:RedSox15@ds251948.mlab.com:51948/heroku_8j8v3k8b";
+
   mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
   require("./routes/routes.js")(app);
     
   // app.listen(process.env.PORT || 3000, function() {  
   app.listen(PORT, function() {
   //   console.log("\n"+"*********************************************");
-    console.log("The server is happily listening on port " + PORT);
+    console.log("\n"+"The server is happily listening on port " + PORT);
     console.log("And is also connected to Mongoose, Yay!");
     console.log('The Collection name is: "mongo-scraperdb"');
     console.log("*********************************************");
