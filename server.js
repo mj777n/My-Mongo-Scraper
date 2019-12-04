2435d0e2484ba424053f4b735f87b7a42d6523ef
M N <mj777norato@gmail.com>
2019-12-04 11:37:23 -0500

    ensuring changes are pushed for Heroku

--- server.js
+++ server.js
@@ -18,15 +18,15 @@ app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
   // Connect to the Mongo DB
-  // var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongo-scraperdb";
+  var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongo-scraperdb";
   // var MONGODB_URI = process.env.MONGODB_URI || "mongodb://user:password@ds251948.mlab.com:51948/heroku_8j8v3k8b";
-  var MONGODB_URI = process.env.MONGODB_URI || "mongodb://heroku_8j8v3k8b:RedSox15@ds251948.mlab.com:51948/heroku_8j8v3k8b";
+  // var MONGODB_URI = process.env.MONGODB_URI || "mongodb://heroku_8j8v3k8b:RedSox15@ds251948.mlab.com:51948/heroku_8j8v3k8b";
   mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
   require("./routes/routes.js")(app);
     
-  app.listen(process.env.PORT || 3000, function() {  
-  // app.listen(PORT, function() {
-    console.log("\n"+"*********************************************");
+  // app.listen(process.env.PORT || 3000, function() {  
+  app.listen(PORT, function() {
+  //   console.log("\n"+"*********************************************");
     console.log("The server is happily listening on port " + PORT);
     console.log("And is also connected to Mongoose, Yay!");
     console.log('The Collection name is: "mongo-scraperdb"');
