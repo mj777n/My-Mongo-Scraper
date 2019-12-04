88567bfad9d0834f69119253a3af909d85e649ed
M N <mj777norato@gmail.com>
2019-12-04 12:18:35 -0500

    ensuring changes are pushed for Heroku

--- server.js
+++ server.js
@@ -17,10 +17,15 @@ app.engine("handlebars", exphbs({ layout: "main" }));
 app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
-  // Connect to the Mongo DB
+//******************************************* */
+  // Connect to the Mongo DB using local host
   // var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongo-scraperdb";
-  // var MONGODB_URI = process.env.MONGODB_URI || "mongodb://user:password@ds251948.mlab.com:51948/heroku_8j8v3k8b";
-  var MONGODB_URI = process.env.MONGODB_URI || "mongodb://heroku_8j8v3k8b:RedSox15@ds251948.mlab.com:51948/heroku_8j8v3k8b";
+ 
+    // Or connect via Heroku using mLabs addon. But this URI is not connecting.
+    // Geting a bunch of "H10" error messages from Heroku Logs. Ughhhhh!!!
+    
+    // var MONGODB_URI = process.env.MONGODB_URI || "mongodb://heroku_8j8v3k8b:RedSox15@ds251948.mlab.com:51948/heroku_8j8v3k8b";
+  var MONGODB_URI = "mongodb://heroku_8j8v3k8b:RedSox15@ds251948.mlab.com:51948/heroku_8j8v3k8b";
   mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
   require("./routes/routes.js")(app);
     
